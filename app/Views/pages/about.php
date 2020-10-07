170ff909286c7b347103753b9d40161200699210
farhanpw <farhan.fpw@gmail.com>
2020-10-07 14:53:31 +0700

    tugas4

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda ipsum ex repellat delectus veritatis voluptatem et blanditiis nemo omnis laborum. Dolor quia recusandae nobis perferendis obcaecati. Ipsum numquam suscipit debitis!</p>
+        </div>
+    </div>
+</div>
