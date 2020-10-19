70562b9cd3b853c80ebb53ea1404c6f77da7379c
farhanpw <farhan.fpw@gmail.com>
2020-10-19 21:31:58 +0700

    Model

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -43,5 +43,6 @@ class BaseController extends Controller
 		// E.g.:
 		// $this->session = \Config\Services::session();
 		$this->nama = 'Farhan';
+		$this->komikModel = new \App\Models\KomikModel();
 	}
 }
---
+++ app/Controllers/Komik.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\KomikModel;
+
+class Komik extends BaseController
+{
+    protected $komikModel;
+    public function __construct()
+    {
+        $this->komikModel = new KomikModel();
+    }
+    public function index()
+    {
+        $komik = $this->komikModel->findAll();
+
+        $data = [
+            'title' => 'Daftar Komik',
+            'komik' => $komik
+        ];
+        // $komikModel = new \App\Models\KomikModel();
+
+
+
+        // cara konek db tanpa model
+        // $db = \Config\Database::connect();
+        // $komik = $db->query("SELECT * FROM komik");
+        // foreach ($komik->getResultArray() as $row) {
+        //     d($row);
+        // }
+
+        return view('komik/index', $data);
+    }
+}
